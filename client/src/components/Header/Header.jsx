4e82e049bb660e37ca2fb9a69e6c971eb2963053
Aleksey Osadchiy <lexaos1@gmail.com>
2020-02-04 14:11:51 +0200

    remove default components

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.sass';
+
+const Header = () => {
+    return(
+        <header className="header">
+        </header>
+    )
+}
+
+export default Header;
